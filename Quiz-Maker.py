beaaa667ca22041d44a76aacc156377c56765247
ChuckyMester <tamasattila.bodnar@icloud.com>
2024-04-03 17:01:32 +0200

    add question  update
    - Add button added
    - add_question method added

--- Quiz-Maker.py
+++ Quiz-Maker.py
@@ -50,14 +50,14 @@ class QuizApp(ctk.CTk):
         self.withdraw()
 
         # Creating the new window
-        manage_questions_window = Toplevel()
-        manage_questions_window.geometry('1000x430')
-        manage_questions_window.minsize(width=1000, height=430)
+        manage_questions_window = ctk.CTkToplevel()
+        manage_questions_window.geometry('800x400')
+        manage_questions_window.minsize(width=800, height=400)
         manage_questions_window.title('Manage questions')
 
         # Question entry
-        question_entry = ctk.CTkEntry(manage_questions_window, placeholder_text="Írja be a kérdést itt", width=400)
-        question_entry.pack(pady=(20, 20))
+        self.question_entry = ctk.CTkEntry(manage_questions_window, placeholder_text="Írja be a kérdést itt", width=400)
+        self.question_entry.pack(pady=(20, 20))
 
         # Answer frame
         answers_frame = ctk.CTkFrame(manage_questions_window)
@@ -70,17 +70,39 @@ class QuizApp(ctk.CTk):
             entry.grid(row=i//2, column=i%2, padx=10, pady=10)
             self.answer_entries.append(entry)
 
-        # Helyes válasz kiválasztása legördülő menüvel
+        # Choose the correct answer combobox
         correct_answer_label = ctk.CTkLabel(manage_questions_window, text="Helyes válasz:")
         correct_answer_label.pack(pady=(0, 5))
 
-        self.correct_answer_var = ctk.StringVar()
-        correct_answer_combobox = ttk.Combobox(manage_questions_window, textvariable=self.correct_answer_var, values=["Válasz 1", "Válasz 2", "Válasz 3", "Válasz 4"], state="readonly", width=30)
-        correct_answer_combobox.pack(pady=(0, 20))
+        self.correct_answer_combobox = ctk.CTkComboBox(manage_questions_window, values=["Válasz 1", "Válasz 2", "Válasz 3", "Válasz 4"], state="readonly")
+        self.correct_answer_combobox.pack(pady=(0, 20))
+
+        add_question_button = ctk.CTkButton(manage_questions_window, text="Add question to database", command=self.add_question_to_database, width=120, height=40)
+        add_question_button.pack(pady=20)
 
         # Back to main menu button
         close_button = ctk.CTkButton(manage_questions_window, text="Go back", width=80, command=lambda: self.close_and_show(manage_questions_window))
         close_button.place(relx=0.03, rely=0.97, anchor='sw')
+
+
+    def add_question_to_database(self):
+        # Collecting the data from all entry field
+        question_text = self.question_entry.get()
+        answers = [entry.get() for entry in self.answer_entries]
+        correct_answer = self.correct_answer_combobox.get()
+
+        # Input check
+        if not question_text or not all(answers) or not correct_answer:
+            print("Missing Data!")
+            return
+
+        print(question_text, answers, correct_answer)
+
+        # Reset all field for new inputs
+        self.question_entry.delete(0, 'end')
+        for entry in self.answer_entries:
+            entry.delete(0, 'end')
+        self.correct_answer_combobox.set('')
 
 
     # Close window and go back to main menu
